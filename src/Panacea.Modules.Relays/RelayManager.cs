f309ae3e55a278802bdfaef123e078c65ab9eb16
Portno <j.remoundos@dotbydot.gr>
2019-07-08 13:53:57 +0300

    fixes

---
+++ src/Panacea.Modules.Relays/RelayManager.cs
@@ -0,0 +1,68 @@
+﻿using Panacea.Core;
+using Panacea.Modularity.Relays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modules.Relays
+{
+    class RelayManager : IRelayManager
+    {
+        private readonly PanaceaServices _core;
+        IRelayModule _blindsModule;
+
+        public RelayManager(PanaceaServices core)
+        {
+            _core = core;
+            _core.PluginLoader.PluginLoaded += PluginLoader_PluginLoaded;
+            _core.PluginLoader.PluginUnloaded += PluginLoader_PluginUnloaded;
+            BlindsAttached = _core.PluginLoader.GetPlugins<IRelayModulePlugin>().Any(p => p.GetType().Name == "FtdiPlugin");
+            if (BlindsAttached)
+            {
+                _core.PluginLoader.GetPlugins<IRelayModulePlugin>().First(p => p.GetType().Name == "FtdiPlugin").GetModuleAsync()
+                    .ContinueWith(task => _blindsModule = task.Result);
+            }
+        }
+
+        private async void PluginLoader_PluginUnloaded(object sender, Modularity.IPlugin e)
+        {
+            BlindsAttached = _core.PluginLoader.GetPlugins<IRelayModulePlugin>().Any(p => p.GetType().Name == "FtdiPlugin");
+            if (BlindsAttached)
+            {
+                _blindsModule = await _core.PluginLoader.GetPlugins<IRelayModulePlugin>().First(p => p.GetType().Name == "FtdiPlugin").GetModuleAsync();
+            }
+            //NurseCallAttached = _core.PluginLoader.GetPlugins<IRelayModulePlugin>().Any(p => p.GetType().Name == "FtdiPlugin");
+        }
+
+        private async void PluginLoader_PluginLoaded(object sender, Modularity.IPlugin e)
+        {
+            BlindsAttached = _core.PluginLoader.GetPlugins<IRelayModulePlugin>().Any(p => p.GetType().Name == "FtdiPlugin");
+            if (BlindsAttached)
+            {
+                _blindsModule = await _core.PluginLoader.GetPlugins<IRelayModulePlugin>().First(p => p.GetType().Name == "FtdiPlugin").GetModuleAsync();
+            }
+            //NurseCallAttached = _core.PluginLoader.GetPlugins<IRelayModulePlugin>().Any(p => p.GetType().Name == "FtdiPlugin");
+        }
+
+        public bool BlindsAttached { get; set; }
+
+        public bool NurseCallAttached { get; set; }
+
+        public Task<bool> SetBlindsDownAsync(bool on)
+        {
+            return _blindsModule.SetStatusAsync(on, 3);
+        }
+
+        public Task<bool> SetBlindsUpAsync(bool on)
+        {
+            return _blindsModule.SetStatusAsync(on, 4);
+        }
+
+        public Task<bool> SetNurseCallAsync(bool on)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ src/Panacea.Modules.Relays/RelaysPlugin.cs
@@ -0,0 +1,50 @@
+﻿using Panacea.Core;
+using Panacea.Modularity.Relays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modules.Relays
+{
+    public class RelaysPlugin : IRelayManagerPlugin
+    {
+        public RelaysPlugin(PanaceaServices core)
+        {
+            _core = core;
+        }
+
+        public Task BeginInit()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public Task EndInit()
+        {
+            return Task.CompletedTask;
+        }
+
+        IRelayManager _manager;
+        private readonly PanaceaServices _core;
+
+        public IRelayManager GetRelayManager()
+        {
+            if (_manager == null)
+            {
+                _manager = new RelayManager(_core);
+            }
+            return _manager;
+        }
+
+        public Task Shutdown()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
